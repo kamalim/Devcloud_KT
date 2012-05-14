d544b6a9189cd71131544b1a2629d3ec82e37394
kamalim <kamalim@chefclient02.(none)>
2012-05-14 16:01:29 +0530

    updated execution cookbook with utar recipe

--- cookbooks/execution-0.0.1/recipes/default.rb
+++ cookbooks/execution-0.0.1/recipes/default.rb
@@ -12,10 +12,10 @@ execute "wget" do
 	command "wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz"
 	not_if "test -f /tmp/ruby-1.9.3-p194.tar.gz"
 end
-
-#execute "tar" do 
-#	cwd "/tmp"
-#	command "tar -zxvf ruby-1.9.3-p194.tar.gz"
-#	end
-#end
+execute "tar" do 
+	cwd "/tmp"
+	command "tar -zxvf ruby-1.9.3-p194.tar.gz"
+	not_if "test -d /tmp/ruby-1.9.3-p194"
+	
+end
 
