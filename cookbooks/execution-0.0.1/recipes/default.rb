23826b36616b23f133537a4960ea7a16a81e61ef
root <root@myclient01.(none)>
2012-05-14 10:27:03 +0530

    added cookbook execution

---
+++ cookbooks/execution-0.0.1/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: execution
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "wget" do
+	cwd "/tmp"
+	command "wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz"
+	not_if "test -f /tmp/ruby-1.9.3-p194.tar.gz"
+end
+
+#execute "tar" do 
+#	cwd "/tmp"
+#	command "tar -zxvf ruby-1.9.3-p194.tar.gz"
+#	end
+#end
+
